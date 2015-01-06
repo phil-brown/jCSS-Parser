a9df9c17c9d5724e72813aad4969e7d7b6dca8dd
Phil Brown <philip22brown@gmail.com>
2015-01-05 21:36:57 -0600

    Major refactor and added support for Maven

---
+++ CSS Parser/src/main/java/self/philbrown/cssparser/App.java
@@ -0,0 +1,14 @@
+package self.philbrown.cssparser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("Running Test");
+        Tester.main(args);
+    }
+}
